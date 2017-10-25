3af067056913686de2a5e7b5b1cafe94f8df7f02
Jone Smith <zxzx25952@gmail.com>
2017-10-25 14:06:42 +0800

    add text stream

--- chatbot_website/chatbot_website/settings.py
+++ chatbot_website/chatbot_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['CHATBOT_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zxzx74147.oicp.net']
 
 
 # Application definition
